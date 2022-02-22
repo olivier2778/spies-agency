d6d932d7052f392da078562ea0e5dd0bc823b53e
Olivier <delpouve@club-internet.fr>
2022-02-22 11:55:35 +0100

    change form

--- src/Form/AgentType.php
+++ src/Form/AgentType.php
@@ -3,38 +3,54 @@
 namespace App\Form;
 use App\Entity\Speciality;
 use App\Entity\Agent;
-use App\Entity\Mission;
+use App\Entity\Nationality;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
-
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType ;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AgentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('agent_last_name')
-            ->add('agent_first_name')
+        $builder           
+            
+            ->add('agent_last_name' , TextType::class, [
+                'label'=>'Nom',
+                ])
+            
+            ->add('agent_first_name' , TextType::class, [
+                'label'=>'Prénom',
+                ])
+
             ->add('agent_birth_date' ,  DateType::class,[
                 'widget' => 'choice',
                 'format' => 'd-M-y',
                 'years' => range(date("Y") - 60, date("Y") - 10) ,
+                'label'=>'Date de Naissance',
                 ])
-            ->add('agent_identification_code')
-            ->add('nationality')
+            
+            ->add('agent_identification_code' , TextType::class, [
+                'label'=>'Code d\'Identification',
+                ])
+            
+            ->add('nationality', EntityType::class, [
+                'class' => Nationality::class,
+                'choice_label' => 'nationality_name',
+                'label'=>'Nationalité',
+                'required' => true,
+            ])
+
             ->add('specialities' , EntityType::class, [
                 'choice_label' => 'speciality_name', 
                 'class' => Speciality::class,
+                'label'=>'Spécialité',
                 'mapped' => true,
                 'multiple' => true,
                 'expanded' => true,
-            ])
-        
+            ])        
         ;
     }
 
